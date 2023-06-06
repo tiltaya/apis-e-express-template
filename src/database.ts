72d4b27166fbd74ae4f52e0896405c02dba4221c
tiltaya <114702626+tiltaya@users.noreply.github.com>
2023-06-05 21:54:41 -0300

    Aula e exercícios

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { COURSE_STACK, TCourse } from "./types"
+import { COURSE_STACK, TCourse, TStudent } from "./types"
 
 export const courses: TCourse[] = [
     {
@@ -18,5 +18,23 @@ export const courses: TCourse[] = [
         name: "Express",
         lessons: 5,
         stack: COURSE_STACK.BACK
+    }
+]
+
+export const students: TStudent[] = [
+    {
+        id: "s001",
+        name: "Taya",
+        age: 27
+    },
+    {
+        id: "s002",
+        name: "Tilápia",
+        age: 11
+    },
+    {
+        id: "s003",
+        name: "Eddie",
+        age: 1
     }
 ]
